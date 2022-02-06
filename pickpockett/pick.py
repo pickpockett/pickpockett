77cabd7eab6ff66555a31adf5ed19f492fcb296d
Mystic-Mirage <mm@m10e.net>
2022-02-07 00:44:55 +0200

    Correct the magnet link extraction

--- pickpockett/pick.py
+++ pickpockett/pick.py
@@ -1,12 +1,10 @@
 import json
-import re
 from typing import Dict, List, cast
 from urllib.parse import parse_qs, urlparse
 
 import requests
+from bs4 import BeautifulSoup
 from requests.utils import dict_from_cookiejar
-
-magnet = re.compile(r"(['\"])(magnet:\?.*xt=urn:btih:[a-fA-F0-9]{40}.*?)\1")
 
 
 def _prepare_cookies(cookies):
@@ -35,16 +33,26 @@ def _prepare_cookies(cookies):
     raise ValueError(f"Wrong cookie {cookies_orig!r}")
 
 
+def _magnet_link(tag):
+    return (
+        tag.name == "a"
+        and tag.has_attr("href")
+        and tag["href"].startswith("magnet")
+    )
+
+
 def find_magnet_link(url, cookies=None):
     req_cookies = _prepare_cookies(cookies) if cookies else None
+
     r = requests.get(url, cookies=req_cookies)
-    match = magnet.search(r.text)
-    if match:
-        res_cookies = dict_from_cookiejar(r.cookies)
-        return (
-            match.group(2),
-            json.dumps(res_cookies) if cookies else None,
-        )
+    bs = BeautifulSoup(r.text)
+    tag = bs.find(_magnet_link)
+
+    if tag:
+        if cookies and (res_cookies := dict_from_cookiejar(r.cookies)):
+            cookies = json.dumps(res_cookies)
+        return tag["href"], cookies
+
     return None, None
 
 
