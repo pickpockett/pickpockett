027d7ad3746b648fa99b4ac94376133c21a8e31f
Mystic-Mirage <mm@m10e.net>
2022-02-08 22:54:51 +0200

    Lower default date precision

--- pickpockett/models.py
+++ pickpockett/models.py
@@ -18,7 +18,5 @@ class Source(db.Model):
     cookies = db.Column(db.Text, nullable=False, server_default="")
     hash = db.Column(db.String(40), nullable=False, server_default="")
     datetime = db.Column(
-        db.DateTime,
-        nullable=False,
-        server_default="0001-01-01 0:00:00.000",
+        db.DateTime, nullable=False, server_default="0001-01-01 00:00:00"
     )
