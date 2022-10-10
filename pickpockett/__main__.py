0a1a0aa28441304db5fc91621bfdd5f9f5d8bf70
Mystic-Mirage <mm@m10e.net>
2022-10-10 14:30:31 +0300

    Add DEBUG option

--- pickpockett/__main__.py
+++ pickpockett/__main__.py
@@ -1,3 +1,4 @@
+import os
 import socket
 
 import waitress
@@ -6,7 +7,9 @@ from werkzeug.serving import get_interface_ip
 
 from . import app
 
-if (hostname := get_interface_ip(socket.AF_INET)) == "127.0.0.1":
+if os.environ.get("DEBUG") not in (None, "0"):
+    hostname = "127.0.0.1"
+elif (hostname := get_interface_ip(socket.AF_INET)) == "127.0.0.1":
     hostname = "0.0.0.0"
 
 waitress.serve(TransLogger(app), host=hostname, port=9119)
